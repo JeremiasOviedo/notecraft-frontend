a8d3473cd505c820b441175276b44e577f593af3
JeremiasOviedo <JeremiasOviedo.Dev@gmail.com>
2023-11-26 05:41:01 -0300

    added animations on technologies icons.

--- src/components/TechnologyCard.tsx
+++ src/components/TechnologyCard.tsx
@@ -1,15 +1,35 @@
-import React, { ReactNode } from "react";
+"use client";
+import React, { ReactNode, useState } from "react";
 
-interface TechnologyCardProps{
-    icon: ReactNode;
-    text: string;
+interface TechnologyCardProps {
+  icon: ReactNode;
+  text: string;
 }
 
-const TechnologyCard: React.FC<TechnologyCardProps> = ({ icon, text}) => {
+const TechnologyCard: React.FC<TechnologyCardProps> = ({ icon, text }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="border-8 rounded-2xl border-white w-48 h-48  hover:border-[#3C6997] fill-white p-6 flex ">
-        {icon}
+    <div className ='flex flex-col justify-center items-center'>
+    <div
+      className={`border-8 rounded-2xl border-white w-48 h-48  hover:border-[#3C6997] fill-white p-6 flex ${
+        isHovered ? 'animate-pulse' : ''
+      }`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {icon}
+   
     </div>
+    <div
+        className={` bg-transparent p-6 opacity-0 transition-opacity font-mono font-bold text-[#3C6997] text-[26px] ${
+          isHovered ? 'opacity-100 animate-pulse' : ''
+        }`}
+      >
+        <p className="m-0">{text}</p>
+      </div>
+    </div>
+
   );
 };
 
