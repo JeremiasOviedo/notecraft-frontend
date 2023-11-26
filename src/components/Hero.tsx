efe4ecc7fa525f12c9b19eb973e1d873b3841874
JeremiasOviedo <JeremiasOviedo.Dev@gmail.com>
2023-11-26 04:48:02 -0300

    changed svg colour.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,14 +5,14 @@ const Hero = () => {
     <div className="flex justify-center items-center place-space-around bg-gradient-to-r from-gray-700 via-gray-900 to-black m-0 pt-[160px] pb-[100px]  ">
       
       <div className="flex flex-col flex-1 m-0">
-        <span className="font-sans font-bold text-[40px] text-white text-center">
+        <span className="font-sans font-bold text-[40px] text-white text-center m-2">
           <h1>Capture ideas, shape your thoughts.</h1>
         </span>
-        <span className="font-sans font-bold text-[50px] text-center text-white ">
+        <span className="font-sans font-bold text-[50px] text-center text-white m-2 ">
           <h1>Welcome to Notecraft.</h1>
           
         </span>
-        <span className='font-sans font-bold text-[22px] text-center '>
+        <span className='font-sans font-bold text-[22px] text-center m-2'>
         <Button to="/a">Get Started!</Button>
         </span>
       </div>
