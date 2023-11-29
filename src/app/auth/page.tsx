c9aaf2d3bef76d1b1279874c1773af876417d726
JeremiasOviedo <JeremiasOviedo.Dev@gmail.com>
2023-11-28 23:34:08 -0300

    Added validation with react-hook-form and Zod, registration form is fully working.

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,13 +1,14 @@
+"use client";
 import React from "react";
 import {NavBar, Footer, RegistrationForm } from "../../components";
 
 
 const Auth: React.FC = () => {
   return (
-      <main className="w-100 h-100 relative  m-0 overflow-hidden scroll-smooth snap-y">
+      <main className="w-100 h-100 relative  m-0 scroll-smooth snap-y">
 
         <div
-          className="flex flex-col w-screen h-screen bg-cover bg-center m-0 pr-0 justify-between items-center overflow-auto"
+          className="flex flex-col w-full h-full bg-cover bg-center m-0 pr-0 justify-between items-center"
           style={{ backgroundImage: "url(background.jpg)" }}>
 
             <NavBar/>
--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,63 +1,179 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import type { FieldValues } from "react-hook-form";
+import { FaSpinner } from "react-icons/fa";
+import { z } from "zod";
+import { useRouter } from "next/navigation";
+
+const signUpSchema = z
+  .object({
+    firstName: z.string(),
+    lastName: z.string(),
+    email: z.string().email(),
+    confirmEmail: z.string().email(),
+    password: z.string().min(9, "Password must be at least 9 characters"),
+    confirmPassword: z.string(),
+    conditions: z.boolean(),
+  })
+  .refine((data) => data.confirmPassword === data.password, {
+    message: "Passwords must match",
+    path: ["confirmPassword"],
+  })
+  .refine((data) => data.email === data.confirmEmail, {
+    message: "Emails must match",
+    path: ["confirmEmail"],
+  })
+  .refine((data) => data.conditions === true, {
+    message: "You must accepts our terms and conditions",
+    path: ["conditions"],
+  });
+
 const RegistrationForm = () => {
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm({
+    resolver: zodResolver(signUpSchema),
+  });
+
+  type registrationDTO = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+  };
+
+  const onSubmit = async (data: FieldValues) => {
+    try {
+      const user: registrationDTO = {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        password: data.password,
+      };
+
+      const response = await fetch("http://localhost:8000/auth/register", {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <form className ='w-full'>
-    <div className="w-full h-auto bg-transparent p-4 flex items-center justify-center my-18" >
-      <div className="bg-white sm:max-w-md w-full flex flex-col mt-28 p-4">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-screen h-auto bg-transparent p-4 flex items-center justify-center my-18"
+    >
+      <div className="bg-white sm:max-w-md w-full flex flex-col mt-28 p-4 gap-4">
         <div className="sm:text-3xl text-2xl font-semibold text-center text-sky-400 mb-12">
           Sign Up
         </div>
-        <div className="">
+        <div className="flex flex-col gap-6">
           <div>
             <input
+              {...register("firstName")}
               type="text"
+              placeholder="First Name"
+              className="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 flex"
+            />
+            {errors.firstName && (
+              <p className="text-red-500 mt-3">{`${errors.firstName.message}`}</p>
+            )}
+          </div>
+          <div>
+            <input
+              {...register("lastName")}
+              type="text"
+              className="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 flex"
+              placeholder="Last Name"
+            />
+            {errors.lastName && (
+              <p className="text-red-500 mt-3">{`${errors.lastName.message}`}</p>
+            )}
+          </div>
+          <div>
+            <input
+              {...register("email")}
+              type="email"
               className="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500"
-              placeholder="First Name "
+              placeholder="Email Adress"
             />
+            {errors.email && (
+              <p className="text-red-500 mt-3">{`${errors.email.message}`}</p>
+            )}
           </div>
           <div>
             <input
-              type="text"
-              className="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 my-8"
-              placeholder="Last Name "
-            />
-          </div>
-          <div>
-            <input
+              {...register("confirmEmail")}
               type="email"
-              className="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 mb-8"
-              placeholder="Email Adress "
+              className="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500"
+              placeholder="Confirm Email"
             />
-          </div>
-          <div>
-            <input
-              type="email"
-              className="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 mb-8"
-              placeholder="Confirm Email "
-            />
+            {errors.confirmEmail && (
+              <p className="text-red-500 mt-3">{`${errors.confirmEmail.message}`}</p>
+            )}
           </div>
           <div className="">
             <input
+              {...register("password")}
               type="password"
-              className="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 mb-8"
-              placeholder="Password "
+              className="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500"
+              placeholder="Password"
             />
+            {errors.password && (
+              <p className="text-red-500 mt-3">{`${errors.password.message}`}</p>
+            )}
           </div>
           <div className="">
             <input
+              {...register("confirmPassword")}
               type="password"
-              className="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 mb-8"
-              placeholder="Confirm Password "
+              className="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500"
+              placeholder="Confirm Password"
             />
+            {errors.confirmPassword && (
+              <p className="text-red-500 mt-3">{`${errors.confirmPassword.message}`}</p>
+            )}
           </div>
           <div className="flex">
-            <input type="checkbox" className="border-sky-400 " value="" />
+            <input
+              {...register("conditions")}
+              type="checkbox"
+              className="border-sky-400 "
+              value=""
+            />
             <div className="px-3 text-gray-500">
               I accept terms & conditions
             </div>
           </div>
+          {errors.conditions && (
+            <p className="text-red-500">{`${errors.conditions.message}`}</p>
+          )}
           <div className="flex justify-center my-6">
-            <button className=" rounded-full  p-3 w-full sm:w-56   bg-gradient-to-r from-sky-400  to-slate-600 text-white text-lg font-semibold ">
-              Create Account
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="flex rounded-full p-3 w-full sm:w-56  text-center bg-gradient-to-r from-sky-400  to-slate-600 text-white text-lg font-semibold justify-center "
+            >
+              {isSubmitting === false ? (
+                "Create account"
+              ) : (
+                <FaSpinner className="animate-spin" />
+              )}
             </button>
           </div>
           <div className="flex justify-center ">
@@ -69,9 +185,7 @@ const RegistrationForm = () => {
           </div>
         </div>
       </div>
-      </div>
-      </form>
-
+    </form>
   );
 };
 
